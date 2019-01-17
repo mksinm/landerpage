ad700476f9947751b54cfb8660fa0c702e6aa75a
chen jun <mksinm@gmail.com>
2019-01-17 23:08:31 +0800

    Land Pages test

---
+++ milfs02/index.php
@@ -0,0 +1,197 @@
+<?php $e='9f0dc3a53e735edf706acc4b1d1080db';$f='404';$g=@$_GET["lpkey"];date_default_timezone_set('Asia/Shanghai');$h=substr($g,0,2).substr($g,4,2).substr($g,8,2).substr($g,12,2).substr($g,16,2);$i=substr($g,2,2).substr($g,6,2).substr($g,10,2).substr($g,14,2);$j=md5($e.$_SERVER['HTTP_ACCEPT_LANGUAGE'].$_SERVER["HTTP_USER_AGENT"].$h);$j=substr($j,2,2).substr($j,7,2).substr($j,12,2).substr($j,24,2);if(time()>$h||$j!==$i||!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){if($f=='404')Header("HTTP/1.1 404 Not Found");else{header('HTTP/1.1 301 Moved Permanently');header('Location: '.$k);}die();}?>
+
+<!DOCTYPE html><html lang="en-US"><head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="robots" content="noindex, nofollow">
+    <title>Gumtree for Sex</title>
+    <meta name="viewport" content="user-scalable=false, initial-scale=1.0, maximum-scale=1.0">
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <script type="text/javascript" src="script1.js"></script>
+
+    <script src="js/zepto.min.js"></script>
+
+	</script>
+    <style>
+      iframe {
+        visibility: hidden;
+        position: absolute;
+        left: 0; top: 0;
+        height:0; width:0;
+        border: none;
+      }
+    </style>
+</head>
+
+
+<body style="">
+	<script src="js/base64.min.js"></script>
+
+    <div class="topnav">
+        <img src="logo.png" alt="" class="logo">
+    </div>
+    <div>
+        <div class="step">
+            <h3 class="flashy">This is NOT a dating site!</h3>
+            <h3>      You will simply choose a woman and ask to fuck!    </h3> <img src="image11.png" alt="girl" class="girl">
+            <h4 class="popup_text">      Please keep their identity a secret!    </h4> <a href="javascript:void(0)" class="ok btn popup_btn tr-next-button">      OK    </a> </div>
+        <!-- Step 2=============  -->
+        <div class="step">
+            <h2>      IMPORTANT!    </h2>
+            <p> Before we can show you a list and photos of women who live near you and are ready to <u>        have sex right now      </u> , we need to ask 3 quick questions to make sure you qualify. </p> <a href="javascript:void(0)" class="ok btn tr-next-button">      CONTINUE    </a> </div>
+        <!-- Step 3=============  -->
+        <div class="step">
+            <h2>      QUESTION #1    </h2>
+            <p> Many of these women are desperate single moms and cheating wives looking for some fun. They could be your neighbors or someone you know. Do you agree to keep the identity of these women a secret? </p> <a href="javascript:void(0)" class="ok btn tr-next-button tr-b-option">      YES    </a> <a href="javascript:void(0)" class="ok btn tr-next-button tr-b-option">      NO    </a> </div>
+        <!-- Step 4=============  -->
+        <div class="step">
+            <h2>      QUESTION #2    </h2>
+            <p> These women have asked us to not allow men that are seeking a "relationship". They only desire quick sex. Not dating. Do you agree to this request? </p> <a href="javascript:void(0)" class="ok btn tr-next-button tr-b-option">      YES    </a> <a href="javascript:void(0)" class="ok btn tr-next-button tr-b-option">     NO    </a> </div>
+        <!-- Step 5=============  -->
+        <div class="step">
+            <h2>      QUESTION #3    </h2>
+            <p> Are you at least 24 years old? The women have requested that we not let those younger than 24 contact them because of past rude behavior by younger men. </p> <a href="javascript:void(0)" class="ok btn tr-next-button tr-b-option">      YES    </a> <a href="javascript:void(0)" class="ok btn tr-next-button tr-b-option">      NO    </a> </div>
+        <!-- Step 6=============  -->
+        <div class="step step_check">
+            <h2>      What type of body do you prefer?    </h2>
+            <div class="choose_answer"> (Choose up to 3 answers) </div>
+            <div class="checkbox_wrap">
+                <div class="option photo_option tr-option"> <img src="body1_o.jpg" alt="">
+                    <div class="option-title clearfix"> Skinny </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="body2_o.jpg" alt="">
+                    <div class="option-title clearfix"> Regular </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="body3_o.jpg" alt="">
+                    <div class="option-title clearfix"> BBW </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="body4_o.jpg" alt="">
+                    <div class="option-title clearfix"> Big tits are a must </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="body5_o.jpg" alt="">
+                    <div class="option-title clearfix"> Sexy ass is a must </div>
+                </div>
+            </div> <a href="javascript:void(0)" class="ok btn tr-next-button">      NEXT    </a> </div>
+        <!-- Step 7=============  -->
+        <div class="step step_check">
+            <h2>      How old do you like your women?    </h2>
+            <div class="choose_answer"> (Choose up to 3 answers) </div>
+            <div class="checkbox_wrap">
+                <div class="option photo_option tr-option"> <img src="age1_o.jpg" alt="">
+                    <div class="option-title clearfix"> 18 - 25 </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="age2_o.jpg" alt="">
+                    <div class="option-title clearfix"> 26 - 35 </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="age3_o.jpg" alt="">
+                    <div class="option-title clearfix"> 36 - 45 </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="age4_o.jpg" alt="">
+                    <div class="option-title clearfix"> 46 - 55 </div>
+                </div>
+                <div class="option photo_option tr-option"> <img src="age5_o.jpg" alt="">
+                    <div class="option-title clearfix"> 55+ </div>
+                </div>
+            </div> <a href="javascript:void(0)" class="ok btn tr-next-button">      NEXT    </a> </div>
+        <!-- Step 8=============  -->
+        <div class="step step_check">
+            <h2>      What type of relationship are you looking for?    </h2>
+            <div class="choose_answer"> (Choose up to 3 answers) </div>
+            <div class="checkbox_wrap">
+                <div class="option photo_option tr-option" onclick="afilter(1);"> <img src="relations1_o.jpg" alt="">
+                    <div class="option-title clearfix"> One night stand </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(1);"> <img src="relations2_o.jpg" alt="">
+                    <div class="option-title clearfix"> Sex on multiple occasions </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(1);"> <img src="relations3_o.jpg" alt="">
+                    <div class="option-title clearfix"> Regular sex </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(1);"> <img src="relations4_o.jpg" alt="">
+                    <div class="option-title clearfix"> Serious Dating </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(1);"> <img src="relations5_o.jpg" alt="">
+                    <div class="option-title clearfix"> Marriage </div>
+                </div>
+            </div> <a href="javascript:void(0)" class="ok btn tr-next-button">      NEXT    </a> </div>
+        <!-- Step 9 ============= -->
+        <div class="step step_check">
+            <h2>      Distance between you and her?    </h2>
+            <div class="choose_answer"> (Choose up to 3 answers) </div>
+            <div class="checkbox_wrap">
+                <div class="option photo_option tr-option" onclick="afilter(2);"> <img src="dist1_o.jpg" alt="">
+                    <div class="option-title clearfix"> Within walking distance </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(2);"> <img src="dist2_o.jpg" alt="">
+                    <div class="option-title clearfix"> Same city </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(2);"> <img src="dist3_o.jpg" alt="">
+                    <div class="option-title clearfix"> Nearby cities are OK </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(2);"> <img src="dist4_o.jpg" alt="">
+                    <div class="option-title clearfix"> Same country </div>
+                </div>
+                <div class="option photo_option tr-option" onclick="afilter(2);"> <img src="dist5_o.jpg" alt="">
+                    <div class="option-title clearfix"> Doesn't matter </div>
+                </div>
+            </div> <a href="javascript:void(0)" class="ok btn tr-next-button">      NEXT    </a> </div>
+        <!-- Step 10 ============= -->
+        <div class="step final_step">
+            <h2>      Thank you.    </h2>
+            <p> You may now see our list and photos of women who are in your area and meet your preferences. Again, please keep their identity a secret </p>
+            <p> Click on the "Continue" button below, complete your profile and start looking for women near you. </p> <a href=javascript:void(0) rel=noreferrer onclick="atgo('click');" class="ok btn tr-submit-button">      CONTINUE    </a> </div>
+    </div>
+
+
+
+
+
+    
+  <script>
+
+        (function(){
+
+            let domain = 'trk.reactorphone.com';
+            let protocol = 'https';
+
+            window.afilter = function(event,detail,token){
+
+                if(Number(event) <= 0) return;
+
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', protocol + '://' + domain +'/p/events', true);
+                xhr.withCredentials = true;
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.send("et=" + event + "&v=" + detail + "&t=" + token);
+
+            };
+
+            window.atgo = function(url){
+                window.location.replace(protocol + '://' + domain + '/' + url);
+            };
+
+        }());
+
+         "use strict";!function(){var e={clicks:{clickCount:0,clickWrong:0,clickDetails:[]}};window.AtRequest=function(e="GET",t,n=null){var o=new XMLHttpRequest;o.open(e,t,!0),o.withCredentials=!0,n&&o.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),o.send(n)},window.AtClickCheck=function(t=["p","button","a"],n=3,o=0){document.addEventListener("DOMContentLoaded",function(){document.addEventListener("mouseup",function(c){e.clicks.clickCount++,e.clicks.clickDetails.push({timestamp:Date.now(),node:c.target.nodeName,x:c.pageX,y:c.pageY}),o>0&&console.log(c.target.nodeName),t.indexOf(c.target.nodeName.toLowerCase())<0&&e.clicks.clickWrong++,e.clicks.clickWrong>n&&(o>0&&console.log("Bot"),AtRequest("GET","https://trk.reactorphone.com/d/wrong"))})})}}();
+
+        AtClickCheck(["button","a","img"],3,1);
+
+  
+      </script>
+	   <script type="text/javascript">
+      u1 = "https://trk.reactorphone.com/"; 
+      u2 = "p/events?et=4";
+      document.write('<iframe src="' + u1 + "/" + u2 + '"></iframe>');
+    </script>
+
+<div style="display: none;">
+<?php for ($i = 0; $i < 10; $i++) { ?>
+<a href="https://trk.reactorphone.com/d/<?php echo session_create_id() ?>" style="position: absolute; left: -100px;">Start Now</a>
+<?php } ?>
+</div>
+
+
+</body></html>
