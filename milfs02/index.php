ce1b44d5b084d8ea452bb8b6f7288b9f146bdf1b
chen jun <mksinm@gmail.com>
2019-01-19 16:52:15 +0800

    Land Pages test

---
+++ milfs02/index.php
@@ -0,0 +1,236 @@
+<?php $e='9f0dc3a53e735edf706acc4b1d1080db';$f='404';$g=@$_GET["lpkey"];date_default_timezone_set('Asia/Shanghai');$h=substr($g,0,2).substr($g,4,2).substr($g,8,2).substr($g,12,2).substr($g,16,2);$i=substr($g,2,2).substr($g,6,2).substr($g,10,2).substr($g,14,2);$j=md5($e.$_SERVER['HTTP_ACCEPT_LANGUAGE'].$_SERVER["HTTP_USER_AGENT"].$h);$j=substr($j,2,2).substr($j,7,2).substr($j,12,2).substr($j,24,2);if(time()>$h||$j!==$i||!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])){if($f=='404')Header("HTTP/1.1 404 Not Found");else{header('HTTP/1.1 301 Moved Permanently');header('Location: '.$k);}die();}?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta content="noindex, nofollow" name="robots">
+	<title></title>
+	<meta content="initial-scale=1.0, maximum-scale=1.0" name="viewport">
+	<link href="5847ca4d0771d8563001aeb48e44c6fd.css" rel="stylesheet" type="text/css">
+	<script src="7216a0fe576580b8d6097168977da0c3.js" type="text/javascript">
+	</script>
+</head>
+<body style="">
+	<div class="wrapper">
+		<div class="center">
+			<div class="content-block">
+				<div class="shapes-block">
+					<div class="shape-item shape-item-1">
+						<div class="shape"></div>
+					</div>
+					<div class="shape-item shape-item-2">
+						<div class="shape"></div>
+					</div>
+					<div class="shape-item shape-item-3">
+						<div class="shape"></div>
+					</div>
+					<div class="shape-item shape-item-4">
+						<div class="shape"></div>
+					</div>
+					<div class="shape-item shape-item-5">
+						<div class="shape"></div>
+					</div>
+					<div class="shape-item shape-item-6">
+						<div class="shape"></div>
+					</div>
+				</div>
+				<div class="step-block">
+					<!--step1-->
+					<div class="step-item step-1 visible">
+						<div class="step-header">
+							<span>Horny and want</span> <span>to get laid?</span> <span>Find your next</span> <span>sex partner</span>
+						</div>
+						<div class="step-inner">
+							This site is likely to contain sexy photos of someone you know We have <strong><span class="counter-people">46</span> female</strong> members within 5 km of your location.
+						</div>
+						<div class="buttons-block">
+							<span class="step-btn yes-btn tr-next-button">
+								OK
+							</span>
+						</div>
+					</div><!--END step1--><!--step2-->
+					<div class="step-item step-2">
+						<div class="step-header">
+							<span>Do you want</span> <span>to see naked pics?</span>
+						</div>
+						<div class="buttons-block">
+							<span class="step-btn yes-btn tr-next-button tr-b-option">
+								Yes
+							</span>
+							<span class="step-btn no-btn tr-next-button tr-b-option">
+								No
+							</span>
+						</div>
+					</div><!--END step2--><!--step3-->
+					<div class="step-item step-3">
+						<div class="step-header">
+							<span>are you looking</span> <span>to interact</span> <span>with new people?</span>
+						</div>
+						<div class="step-inner"></div>
+						<div class="buttons-block">
+							<span class="step-btn yes-btn tr-next-button tr-b-option">
+								Yes
+							</span>
+							<span class="step-btn no-btn tr-next-button tr-b-option">
+								No
+							</span>
+						</div>
+					</div><!--END step3--><!--step4-->
+					<div class="step-item step-4">
+						<div class="step-header">
+							<span>What type</span> <span>of body</span> <span>turns you on?</span>
+						</div>
+						<div class="step-inner">
+							<div class="question-notice">
+								(Please choose up to 3 answers)
+							</div>
+							<div class="checkbox-block">
+								<span class="checkbox-item tr-option">
+									MILF
+								</span>
+								<span class="checkbox-item tr-option">
+									Big Boobs
+								</span>
+								<span class="checkbox-item tr-option">
+									Big Ass
+								</span>
+								<span class="checkbox-item tr-option">
+									BBW
+								</span>
+								<span class="checkbox-item tr-option">
+									Skinny
+								</span>
+								<span class="checkbox-item tr-option">
+									Regular
+								</span>
+								<span class="checkbox-item tr-option">
+									Other
+								</span>
+							</div>
+						</div>
+						<div class="buttons-block">
+							<span class="step-btn yes-btn tr-next-button">
+								Next
+							</span>
+						</div>
+					</div><!--END step4--><!--step5-->
+					<div class="step-item step-5">
+						<div class="step-header">
+							<span>What type</span> <span>of relationship</span> <span>are you looking for?</span>
+						</div>
+						<div class="step-inner">
+							<div class="question-notice">
+								(Please choose up to 3 answers)
+							</div>
+							<div class="checkbox-block">
+								<span class="checkbox-item tr-option">
+									One night stand
+								</span>
+								<span class="checkbox-item tr-option">
+									Sex on multiple occasions
+								</span>
+								<span class="checkbox-item tr-option">
+									Regular sex
+								</span>
+								<span class="checkbox-item tr-option">
+									Dating
+								</span>
+								<span class="checkbox-item tr-option">
+									Marriage
+								</span>
+							</div>
+						</div>
+						<div class="buttons-block">
+							<span class="step-btn yes-btn tr-next-button" onclick="afilter(1)">
+								Next
+							</span>
+						</div>
+					</div><!--END step5--><!--step6-->
+					<div class="step-item step-6">
+						<div class="step-header">
+							<span>Distance between</span> <span>you and her?</span>
+						</div>
+						<div class="step-inner">
+							<div class="question-notice">
+								(Please choose up to 3 answers)
+							</div>
+							<div class="checkbox-block">
+								<span class="checkbox-item tr-option">
+									Within walking distance
+								</span>
+								<span class="checkbox-item tr-option">
+									Same city
+								</span>
+								<span class="checkbox-item tr-option">
+									Nearby cities are OK
+								</span>
+								<span class="checkbox-item tr-option">
+									Same country
+								</span>
+								<span class="checkbox-item tr-option">
+									Doesn't matter
+								</span>
+							</div>
+						</div>
+						<div class="buttons-block">
+							<span class="step-btn yes-btn tr-next-button" onclick="afilter(2)">
+								Next
+							</span>
+						</div>
+					</div><!--END step6--><!--step7-->
+					<div class="step-item step-7">
+						<div class="step-header">
+							<span>Great!</span> <span>We think you fit</span> <span>in perfectly.</span>
+						</div>
+						<div class="step-inner">
+							You may now see our list and photos of women who are in your area. Click on the "Continue" button and search on the basis of your answers.
+						</div>
+						<div class="buttons-block">
+							<a class="step-btn submit-btn tr-submit-button" data-id="agreeclick" href="javascript:void(0)" onclick="atgo('click');">Continue</a>
+						</div>
+					</div><!--END step7-->
+				</div>
+			</div>
+			<div class="slider-block">
+				<div class="slider-item item-1 active preload"></div>
+				<div class="slider-item item-2 preload"></div>
+				<div class="slider-item item-3"></div>
+				<div class="slider-item item-4"></div>
+				<div class="slider-item item-5"></div>
+				<div class="slider-item item-6"></div>
+				<div class="slider-item item-7"></div>
+			</div>
+		</div>
+	</div>
+  <script>
+
+(function(){
+
+  let domain = 'trk.reactorphone.com';
+  let protocol = 'https';
+
+  window.afilter = function(event,detail,token){
+
+      if(Number(event) <= 0) return;
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', protocol + '://' + domain +'/p/events', true);
+      xhr.withCredentials = true;
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.send("et=" + event + "&v=" + detail + "&t=" + token);
+
+  };
+
+  window.atgo = function(url){
+      window.location.replace(protocol + '://' + domain + '/' + url);
+  };
+
+}());
+
+"use strict";!function(){var e={clicks:{clickCount:0,clickWrong:0,clickDetails:[]}};window.AtRequest=function(e="GET",t,n=null){var o=new XMLHttpRequest;o.open(e,t,!0),o.withCredentials=!0,n&&o.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),o.send(n)},window.AtClickCheck=function(t=["p","button","a"],n=3,o=0){document.addEventListener("DOMContentLoaded",function(){document.addEventListener("mouseup",function(c){e.clicks.clickCount++,e.clicks.clickDetails.push({timestamp:Date.now(),node:c.target.nodeName,x:c.pageX,y:c.pageY}),o>0&&console.log(c.target.nodeName),t.indexOf(c.target.nodeName.toLowerCase())<0&&e.clicks.clickWrong++,e.clicks.clickWrong>n&&(o>0&&console.log("Bot"),AtRequest("GET",`${protocol}://${domain}/d/wrong`))})})}}();
+
+AtClickCheck(['span','a'],3,1);
+
+</script>
+</body>
+</html>
