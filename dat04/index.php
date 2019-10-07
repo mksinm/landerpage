1fe0b017cbefe8730a47081aa113297a20ca1300
chen jun <mksinm@gmail.com>
2019-10-07 22:35:01 +0800

    Land Pages test

---
+++ dat04/index.php
@@ -0,0 +1,25 @@
+
+<?php
+$daima = array(
+      array('name'=>'https://111.biznestop.com/dat04/s1.php','weights'=>5),
+      array('name'=>'https://111.biznestop.com/dat04/s2.php','weights'=>5)
+);
+
+$randn = getConsultant($daima);
+setcookie('username',$randn['name'], 0);
+header("Location: {$randn['name']}");
+
+function getConsultant($data){
+	$weight = 0;
+	$users = array();
+	foreach ($data as $one) {
+		$oneWeight = (int)$one['weights'] ? $one['weights'] : 1;
+		$weight += $oneWeight;
+		for ($i = 0; $i < $oneWeight; $i ++) {
+			$users[] = $one;
+		}
+	}
+	return $users[mt_rand(0, $weight-1)];
+}
+
+?>
