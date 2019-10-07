4e764e5fa21e3ae497dad571930256f3d9a96e64
chen jun <mksinm@gmail.com>
2019-10-07 23:36:52 +0800

    Land Pages test

---
+++ dat05/index.php
@@ -0,0 +1,26 @@
+
+<?php
+$daima = array(
+      array('name'=>'https://222.biznestop.com/dat05/s1.php','weights'=>5),
+	  array('name'=>'https://222.biznestop.com/dat05/s2.php','weights'=>5),
+      array('name'=>'https://222.biznestop.com/dat05/s3.php','weights'=>5)
+);
+
+$randn = getConsultant($daima);
+setcookie('username',$randn['name'], 0);
+header("Location: {$randn['name']}");
+
+function getConsultant($data){
+	$weight = 0;
+	$users = array();
+	foreach ($data as $one) {
+		$oneWeight = (int)$one['weights'] ? $one['weights'] : 1;
+		$weight += $oneWeight;
+		for ($i = 0; $i < $oneWeight; $i ++) {
+			$users[] = $one;
+		}
+	}
+	return $users[mt_rand(0, $weight-1)];
+}
+
+?>
---
+++ dat05/s1.php
@@ -0,0 +1,18 @@
+<?php
+define('ROOT_DIR', dirname(__FILE__));
+$tzurl = "https://222.biznestop.com/dat05";
+$includefile = 'four/404.html';
+if(!empty($_COOKIE['username'])){
+setcookie('tzname',$tzurl, 0);
+$includefile = 'four/s1.html';
+}
+
+if(@!file_exists($includefile)) {
+	echo 'Internal Server Error';
+	exit;
+}else{
+	include $includefile;
+}
+
+
+?>
