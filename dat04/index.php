e22e38e378a6d723c91a91447ae2f13a57f1a103
chen jun <mksinm@gmail.com>
2019-10-10 12:55:54 +0800

    Land Pages test

--- dat04/index.php
+++ dat04/index.php
@@ -1,32 +1,50 @@
-
 <?php
-$daima = array(
-      array('name'=>'https://111.biznestop.com/dat04/s1.php','weights'=>5),
-      array('name'=>'https://111.biznestop.com/dat04/s2.php','weights'=>5),
-      array('name'=>'https://111.biznestop.com/dat04/s3.php','weights'=>5),
-	  array('name'=>'https://111.biznestop.com/dat04/s4.php','weights'=>5),
-	  array('name'=>'https://111.biznestop.com/dat04/s5.php','weights'=>5),
-	  array('name'=>'https://111.biznestop.com/dat04/s6.php','weights'=>5),
-	  array('name'=>'https://111.biznestop.com/dat04/s7.php','weights'=>5),
-	  array('name'=>'https://111.biznestop.com/dat04/s8.php','weights'=>5),
-	  array('name'=>'https://111.biznestop.com/dat04/s9.php','weights'=>5)
-);
+require "./predis/autoload.php";
+require "./configureTool.php";
+Predis\Autoloader::register();
+require_once 'common_function.php';
 
-$randn = getConsultant($daima);
-setcookie('username',$randn['name'], 0);
-header("Location: {$randn['name']}");
+date_default_timezone_set('Asia/Shanghai');
+$date =  date('y-m-d h:i:s',time());
 
-function getConsultant($data){
-	$weight = 0;
-	$users = array();
-	foreach ($data as $one) {
-		$oneWeight = (int)$one['weights'] ? $one['weights'] : 1;
-		$weight += $oneWeight;
-		for ($i = 0; $i < $oneWeight; $i ++) {
-			$users[] = $one;
-		}
-	}
-	return $users[mt_rand(0, $weight-1)];
+
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
 }
 
+
+
+function PC_tiao()
+{
+    $arr=array('cd'); 
+    $key=array_rand($arr);
+    $jieshaos=file("$arr[$key].txt");
+    $jieshaoc=count($jieshaos);
+    $jieshaor=rand(0,$jieshaoc-1);
+    $jieshao=@$jieshaos[$jieshaor]; 
+    $tiao = str_replace(array("\r\n", "\r", "\n"), "", $jieshao);
+    Header("Location:$tiao");
+	
+}
+
+	  $ip = get_ip_address();
+    $log = $date."\t".$ip."\n";
+    error_log($log, 3, "./my-errors.log");
+	echo PC_tiao();
 ?>
